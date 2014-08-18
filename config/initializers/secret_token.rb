0b2d7066f981dba3d483af5682715b1e708cd9bb
Jevon Wright <jevon@jevon.org>
2014-08-19 11:55:09 +1200

    Initial generated application
    Generated with `rails new rrw`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rrw::Application.config.secret_key_base = '0fcd6ae440c308be4e84c2678bb11a49c88636b729b4c83e8829522d58cd8cb13733cedff023b9db5c668c3d18cbfd760821982e73520d74c3d28b24ce536cad'
