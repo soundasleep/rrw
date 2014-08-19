9ef455bb830a3d40026b4d52519e628958ca06bf
Jevon Wright <jevon@jevon.org>
2014-08-19 12:23:07 +1200

    Set Player model defaults

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,13 @@
 class Player < ActiveRecord::Base
+  after_initialize :init
+
+  # Initialise model defaults
+  def init
+    self.name ||= "Unnamed"
+    self.level ||= 1
+    self.current_health ||= 0
+    self.total_health ||= 0
+    self.current_mana ||= 0
+    self.total_mana ||= 0
+  end
 end
