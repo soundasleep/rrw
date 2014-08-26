6325837d893fb8bd1749c4728e21a81598f4f9af
Jevon Wright <jevon@jevon.org>
2014-08-26 15:54:37 +1200

    Add test for Npc.character_type.get_model

--- test/models/npc_test.rb
+++ test/models/npc_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class NpcTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "we have npcs to test" do
+    assert_not_equal 0, Npc.all.length || 0
+  end
+
+  # we need to convert ActiveRelation to an Array because ActiveRelation#uniq
+  # is completely different from Enumerable#uniq
+  Npc.all.to_a.uniq { |u| u.character_type }.each do |npc|
+    test "we can load npc #{npc.character_type}" do
+      assert_not_nil npc.get_model
+    end
+  end
+
 end
